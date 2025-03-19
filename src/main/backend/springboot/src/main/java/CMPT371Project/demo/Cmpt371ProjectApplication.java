b853c1bad64ea7db4e4555a1916dca4c491b40c9
jdysay <jdysay@icloud.com>
2025-03-19 15:41:30 -0700

    added springboot project to backend folder

---
+++ src/main/backend/springboot/src/main/java/CMPT371Project/demo/Cmpt371ProjectApplication.java
@@ -0,0 +1,13 @@
+package CMPT371Project.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Cmpt371ProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Cmpt371ProjectApplication.class, args);
+	}
+
+}
---
+++ src/main/backend/springboot/src/test/java/CMPT371Project/demo/Cmpt371ProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package CMPT371Project.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Cmpt371ProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
